8819153818e48872329c7bec1a27634a4e3e6338
khushboo kumari <ksingh200601@gmail.com>
2025-02-11 02:13:15 +0530

    initialzed mvc pattern and connected to the server and db

---
+++ index.js
@@ -0,0 +1,24 @@
+const express=require('express');
+const { route } = require('./router/routes');
+const app=express()
+const mongoose=require('mongoose')
+const Port=3000;
+const dotenv=require('dotenv')
+dotenv.config()
+
+//inbuilt-middleware 
+app.use(express.json())
+app.use(route)
+
+mongoose.connect(process.env.MONOGODB_URL)
+ const db=mongoose.connection
+ db.on('error',()=>{
+    console.log('error while connecting to db')
+ })
+ db.once('open',()=>{
+    console.log('connected to database')
+ })
+//connection to server
+app.listen(Port,()=>{
+    console.log(`server started on ${Port}`)
+})
