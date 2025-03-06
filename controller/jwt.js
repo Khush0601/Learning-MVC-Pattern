866217eb6b584d2f0f523e0c94b4f111e812eb9e
khushboo kumari <ksingh200601@gmail.com>
2025-03-06 10:10:37 +0530

    modified

--- controller/jwt.js
+++ controller/jwt.js
@@ -4,12 +4,16 @@ require('dotenv').config()
 exports.generateToken=async(data)=>{
     try{
         
+
+        
         return jwt.sign(data, process.env.JWT_SECRET, { expiresIn: '1h' });
     }
     catch(err){
         throw new Error("Token generation failed");
         
     }
+
+
 }
 
 exports.decodeToken=(token)=>{
